9c111d555a16974eb4cdd6662d76178b05303760
Tartarus6 <ivanglott@gmail.com>
2026-05-07 12:16:13 +0200

    wrote out idea for turning edge pixels into shapes in shaders.ts

--- src/lib/shaders.ts
+++ src/lib/shaders.ts
@@ -9,6 +9,47 @@ import gradient_max_shader from '$lib/shaders/gradient_maximizing.wgsl?raw';
 import edge_tracing_step_shader from '$lib/shaders/edge_tracing_step.wgsl?raw';
 import { textureToEdgeSvg } from '$lib/edge_svg';
 
+/*
+Idea for turning edges into shapes:
+
+For each pixel with a degree greater than 2 (meaning more than 2 neighbors), say that pixel "points to"
+the pixel that is the soonest clockwise pixel (or counterclockwise it shouldnt matter as long as it's
+consistent).
+
+*Issue*: not sure how to define which direction to start with (easy to tell by looking at it, but not
+sure how to define it)
+
+For Example:
+
+ +-1--2--3--4--5--6-+
+A| ·  ·  · [ ] ·  · |
+B|[ ] ·  · [ ] ·  · |
+C| · [ ] · [ ] ·  · |
+D| ·  · [ ][ ][ ][ ]|
+E| ·  ·  ·  ·  ·  · |
+ +------------------+
+
+We would want to split the pixels above into 3 shapes: the bottom ones, the top left ones, and the top right
+ones. In order to make those shapes, we can connect:
+	Top Left  - A4 <-> B4 <-> C4 <-> D4 <-> D3 <-> C2 <-> B1
+	Bottom    - B1 <-> C2 <-> D3 <-> D4 <-> D5 <-> D6
+	Top Right - D6 <-> D5 <-> D4 <-> C4 <-> B4 <-> A4
+
+In the shapes described above, D4 is treated as a "hub" node, so it is included in all 3. I'm not sure how to
+make sure that an algorithm to make the shapes will include that central pixel instead of, for example, using
+these edges instead (skipping the hub node sometimes).
+	Top Left  - A4 <-> B4 <-> C4 <-> D3 <-> C2 <-> B1
+	Bottom    - B1 <-> C2 <-> D3 <-> D4 <-> D5 <-> D6
+	Top Right - D6 <-> D5 <-> C4 <-> B4 <-> A4
+
+Notable things for an algorithm:
+- In the diagram above, D3, C4, D4, and D5 are the high-degree pixels.
+- D4 is the "hub" pixel. not sure how to identify that, since D3, D4, and D5 all have a degree of 3, and C4
+	even has a degree of 4. So idk how to define a hub node such that it would pick D4.
+- The connections described above aren't closed, but that's just because i wanted a short example. We want for
+	All shapes to be closed loops.
+*/
+
 // TODO: move this const somewhere better
 // TODO: figure out what a good value for this const is
 /// each thread in the mean density score passes will be in charge of summing this many elements
@@ -30,6 +71,7 @@ const partial_sum_size: number = 8;
 // DONE: filter maxima to find only important edges
 // DONE: implement Canny double threshold. Above H, maxima are immediately accepted, and abolve L, pixels are taken if connected to valid points. Below L are ignored. (H and L should be calculated based on image)
 // DONE: switch to including alpha in gradient math, rather than just Lab
+// TODO: add a pass to check whether edge tracing is complete (check if all marked edge pixels have degree of at least 2) (need to do this while avoiding the 100ms waits of cpu-side reads)
 // TODO: combine nodes into edges by "Devernay Sub-Pixel Correction" interpolation (quadratic interpolation of the gradient norm between three neighboring positions along the gradient direction)
 // TODO: figure out how to turn edges into an actual vector image. How will T-intersections be handled? How will color blocks be identified? How will unclosed edges be handled? etc.
 
